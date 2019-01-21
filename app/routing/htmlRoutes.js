4e2c4e1e0d0d33952f59a80dd303d257e90b9d10
Heidijvr <43320863+Heidijvr@users.noreply.github.com>
2019-01-20 23:20:53 -0800

    Fix html routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,14 +3,14 @@ var path = require("path");
 // Exports the variable
 module.exports = function(app) {
     // Sets the route for the home page
-    app.get(function(req, res) {
+    app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname, "/../public/home.html"));
-});
+    });
 
 
- // Sets the route for the about page
- app.get('/survey', function(req, res) {
-    res.sendFile(path.join(__dirname + "/../public/survey.html"));
-});
+    // Sets the route for the about page
+    app.get('/survey', function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/survey.html"));
+    });
 
 }
